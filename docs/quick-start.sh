1246f292f7a6f020a45bee9cb85dbf73ec29788b
Igor Suvorov <suvorov.igor.90@gmail.com>
2017-05-10 18:35:45 +0400

    Update quick-start.sh

--- docs/quick-start.sh
+++ docs/quick-start.sh
@@ -5,7 +5,7 @@ mkdir -p ./.storybook
 wget https://raw.githubusercontent.com/isuvorov/storybox/master/docs/example/addons.js -O ./.storybook/addons.js
 wget https://raw.githubusercontent.com/isuvorov/storybox/master/docs/example/config.js -O ./.storybook/config.js
 wget https://raw.githubusercontent.com/isuvorov/storybox/master/docs/example/glob.txt -O ./.storybook/glob.txt
-wget https://raw.githubusercontent.com/isuvorov/storybox/master/docs/example/params.txt -O ./.storybook/params.txt
+wget https://raw.githubusercontent.com/isuvorov/storybox/master/docs/example/params.js -O ./.storybook/params.js
 wget https://raw.githubusercontent.com/isuvorov/storybox/master/docs/example/webpack.config.js -O ./.storybook/webpack.config.js
 yarn add --dev storybox @kadira/storybook@2.35.3
 echo " "
